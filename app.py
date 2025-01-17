6de1b735d9d0fe290e07b1ef0b0ddb98bf7c40dc
Ishara <isharaashen1234@gmail.com>
2025-01-17 15:10:54 +0530

    Chenge all code

--- app.py
+++ app.py
@@ -44,6 +44,56 @@ def resize():
         )
     except Exception as e:
         return str(e), 500
+    
+
+@app.route('/rapid_resizer')
+def rapid_resizer():
+    return render_template('rapid_resizer.html')
+
+@app.route('/rapid_resize', methods=['POST'])
+def rapid_resize():
+    try:
+        # Get the uploaded file
+        file = request.files['image']
+        if not file:
+            return "No file uploaded!", 400
+
+        # Open the image
+        image = Image.open(file)
+
+        # Fixed width
+        width = 2500
+
+        # Get the aspect ratio
+        aspect_ratio = image.width / image.height
+
+        # Calculate the height based on the aspect ratio and fixed width
+        height = int(width / aspect_ratio)
+
+        # If the calculated height exceeds 1800px, adjust it
+        if height > 1800:
+            height = 1800
+            width = int(height * aspect_ratio)
+
+        # Resize the image with the new width and height
+        resized_image = image.resize((width, height))
+
+        # Save the resized image to memory
+        image_io = BytesIO()
+        resized_image.save(image_io, format=image.format)
+        image_io.seek(0)
+
+        # Send the resized image back to the user
+        return send_file(
+            image_io,
+            mimetype=f'image/{image.format.lower()}',
+            as_attachment=True,
+            download_name=f'rapid_resized_{file.filename}'
+        )
+    except Exception as e:
+        return str(e), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
